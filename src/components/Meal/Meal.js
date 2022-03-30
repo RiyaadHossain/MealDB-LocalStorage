e849755040d17a3f4dc5598703b03b6d8a5992bc
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-30 12:48:53 +0600

    order list is working correctly

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Meal.css';
 
-const Meal = (props) => {
-    const { strMeal, strInstructions, strMealThumb } = props.meal;
+const Meal = ({meal, makeOrder}) => {
+    const { strMeal, strInstructions, strMealThumb } = meal;
     
     return (
         <div className="meal">
             <img src={strMealThumb} alt="" />
             <h4>{strMeal}</h4>
             <p>{strInstructions.slice(0, 100)}</p>
-            <button>Add this Food</button>
+            <button onClick={() => makeOrder(meal)}>Add this Food</button>
         </div>
     );
 };
--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -12,22 +12,12 @@ const Restaurant = () => {
             .then(res => res.json())
             .then(data => setMeals(data.meals));
     }, []);
-    /* 
-        The above api link or the below method will now work for search. 
-        if you want to implement search in this code. 
-        1. add a input field 
-        2. declare a state to keep search field text
-        3. Make meal loading api to dependant on search text
-        4. change the meal loading api.you will get the right api link on their website.
-        5. make the meal loading api dynamic using template string. 
-        6. Also, the useEffect below will not work. Because, search result might not include the meals previously added to the cart
-        7. in that case, for each mealId, you have to load the meal from the api (you will find a new pai to load meal by Id) and then add them to the order state.
-        ---------------  
-        Read carefully, give it a try. [ Ki ache jibone]
-        if  you need help, let us know in the support session
-    */
     
-
+    const makeOrder = meal => {
+        const newOrder = [...orders, meal]
+        setOrders(newOrder)
+    }
+    
     return (
         <div className="restaurant-menu">
             <div className="meals-container">
@@ -35,6 +25,7 @@ const Restaurant = () => {
                     meals.map(meal => <Meal
                         key={meal.idMeal}
                         meal={meal}
+                        makeOrder={makeOrder}
                     ></Meal>)
                 }
             </div>
@@ -47,3 +38,19 @@ const Restaurant = () => {
 };
 
 export default Restaurant;
+
+
+/* 
+    The above api link or the below method will now work for search. 
+    if you want to implement search in this code. 
+    1. add a input field 
+    2. declare a state to keep search field text
+    3. Make meal loading api to dependant on search text
+    4. change the meal loading api.you will get the right api link on their website.
+    5. make the meal loading api dynamic using template string. 
+    6. Also, the useEffect below will not work. Because, search result might not include the meals previously added to the cart
+    7. in that case, for each mealId, you have to load the meal from the api (you will find a new pai to load meal by Id) and then add them to the order state.
+    ---------------  
+    Read carefully, give it a try. [ Ki ache jibone]
+    if  you need help, let us know in the support session
+*/
